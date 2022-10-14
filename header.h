4a24fc728f9c18a79a3be90c95df0baf13550f4b
Ali <100732196+alisogg@users.noreply.github.com>
2022-10-14 00:02:43 -0500

    Create header.h

---
+++ header.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <iostream>
+#include <fstream>
+#include <windows.h>  
+#include <vector>
+#include <string.h>
+#include <iomanip>
+#include <fstream>
+#include <time.h>
+#include "conio.h"
+#include "colores.h"
+#pragma intrinsic(__rdtsc)
+#define NTEST 100000
+using namespace std;
+
+
+#include <locale.h> //Agregar acentos
+#include "sodium.h" //Importar libreria libsodium
+
+//Todas las librerias necesarias para openssl
+#include <openssl/aes.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/err.h>
+#include <openssl/sha.h>
+#pragma warning(disable : 4996)
